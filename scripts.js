6d7cc194241aeec84dde249dab8f692c03e52a8f
javadoffuad <javadoffuad@gmail.com>
2017-09-20 19:16:56 +0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,27 @@
+function tHeadFixer(e){
+    $this = $(e.target);
+    var $table = e.data.table;
+    var top = $this.offset().top;
+    var distance = e.data.distance || 60;
+
+    if($this.scrollTop() > distance && !window.thead2){
+        window.thead2 = $("<div>")
+                .addClass("scroll-thead")
+                .css("top", top);
+
+        $.each($table.find(".table__thead tr").children(), function(){
+            $("<div>").addClass("scroll-thead__item")
+                .text($(this).text())
+                .css("width", $(this).innerWidth())
+                .appendTo(window.thead2);
+        });
+
+        $this.find("> div").append(window.thead2);
+    }
+    else if($this.scrollTop() < distance && window.thead2){
+        $this.find(".scroll-thead").remove();
+        delete window.thead2;
+    }
+}
+
 console.log("Test!!!");
